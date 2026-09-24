1aa34e2e8ca9ecfbcbff3cc6ace508e8412035e0
Tim <37406109+tim32142000@users.noreply.github.com>
2026-09-24 20:14:35 +0800

    Configure API URL for deployment

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,12 @@
+import os
+
 import requests
 import streamlit as st
 
-API_BASE_URL = "http://127.0.0.1:8000"
+API_BASE_URL = os.getenv(
+    "API_BASE_URL",
+    "http://127.0.0.1:8000",
+)
 
 CATEGORIES = [
     "food",
